306251ee08606d40f7d07f17c6f00e4977f7e849
techbhaskarthouti <techbhaskarthouti@gmail.com>
2026-05-23 14:08:51 +0530

    Stage 1: Cubboard changes

--- BASICS/Main.java
+++ BASICS/Main.java
@@ -2,6 +2,27 @@ package BASICS;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Cupboard cupboard = new Cupboard("Blue", 3);
+        System.out.println("Cupboard Color: " + cupboard.getColor());
+        System.out.println("Number of Shelves: " + cupboard.getShelves());
+    }
+}
+
+
+class Cupboard {
+    private String color;
+    private int shelves;
+
+    public Cupboard(String color, int shelves) {
+        this.color = color;
+        this.shelves = shelves;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getShelves() {
+        return shelves;
     }
 }
